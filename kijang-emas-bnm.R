bb6bc4fa1fdff3aa3f4c9ec21cd1ebad12214216
Khairul Fathi <khairulfathi@users.noreply.github.com>
2019-04-06 13:12:26 +0800

    Retrieve 5 years of data 2014:2018

--- kijang-emas-bnm.R
+++ kijang-emas-bnm.R
@@ -8,21 +8,29 @@ require("jsonlite")
 require("dplyr")
 require("tidyverse")
 
-# call 2018 data
-data_2018 <- data.frame()
+# initialize empty data frame
+data <- data.frame()
 
-for(i in 1:12)
+for(y in 2014:2018)
 {
-  resp_2018 <- GET(paste("https://api.bnm.gov.my/public/kijang-emas/year/2018/month/", i, sep = ""), add_headers(Accept = "application/vnd.BNM.API.v1+json"), user_agent("insomnia/6.3.2"))
-  text_2018 <- content(resp_2018, "text")
-  json_2018 <- fromJSON(text_2018, flatten = TRUE)
+  for(m in 1:12)
+  {
+    resp <- GET(paste("https://api.bnm.gov.my/public/kijang-emas/year/", y, "/month/", m, sep = ""), add_headers(Accept = "application/vnd.BNM.API.v1+json"), user_agent("insomnia/6.3.2"))
+    text <- content(resp, "text")
+    json <- fromJSON(text, flatten = TRUE)
+    
+    data <- bind_rows(data, json)
+    
+    m <- m + 1
+  }
   
-  data_2018 <- bind_rows(data_2018, json_2018)
-  
-  i <- i + 1
+  Sys.sleep(10) # to avoid API limit error 
+  y <- y + 1
 }
 
-one_selling <- na.omit(data_2018[c(1,3)])
-one_selling$effective_date <- as.Date(one_selling$effective_date, "%Y-%m-%d")
+# drop un-necessary variables and empty rows
+data <- data[!names(data) %in% c("last_updated", "total_result")]
+data <- na.omit(data)
 
-ggplot(one_selling, aes(effective_date, one_oz.selling)) + geom_point()
+# format effective_date as Date class
+data$effective_date <- as.Date(data$effective_date, "%Y-%m-%d")
